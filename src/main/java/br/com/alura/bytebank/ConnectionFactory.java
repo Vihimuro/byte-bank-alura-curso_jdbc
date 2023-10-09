985ee27cf2fb0a07e511d890fe3998bf5ecf9704
vihimuro <vihi1208@gmail.com>
2023-10-09 15:03:54 -0400

    feat: lesson 02 - information registration

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -4,17 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionDB {
-    public static void main(String... x) {
+public class ConnectionFactory {
 
+    public Connection recoverConnection() {
         try{
-            Connection connection = DriverManager
+            return DriverManager
                     .getConnection("jdbc:postgresql://localhost:5432/byte_bank?user=postgres&password=password");
-            System.out.println("Connected");
-            connection.close();
         }
         catch (SQLException e) {
-            System.out.println(e);
+            throw new RuntimeException(e);
         }
     }
 }
